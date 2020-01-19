ed01155deac7d214bfd03f5098270d1fa15289c3
kaoZ57 <57824756+kaoZ57@users.noreply.github.com>
2020-01-19 10:11:31 +0700

    Update TXT.go

--- TXT.go
+++ TXT.go
@@ -29,3 +29,33 @@ func fileCreate() {
 		return
 	}
 }
+
+func copy1(wg *sync.WaitGroup) {
+	defer wg.Done()
+	file, err := os.Create("./TXT/1.txt")
+	if err != nil {
+		return
+	}
+	for x := 1; x <= 1000; x++ {
+		defer file.Close()
+		file.WriteString("Atiwan\nPhongam\n25\n")
+	}
+	for i := 1; i <= 750; i++ {
+		sFile, err := os.Open(fmt.Sprintf("./TXT/%v.txt", i))
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer sFile.Close()
+
+		eFile, err := os.Create(fmt.Sprintf("./TXT/%v.txt", i+1))
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer eFile.Close()
+
+		_, err = io.Copy(eFile, sFile)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+}
