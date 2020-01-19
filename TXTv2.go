5840b3a3acd7dbdbf4df8854c4e33391b30d1001
kaoZ57 <57824756+kaoZ57@users.noreply.github.com>
2020-01-19 10:13:23 +0700

    Update TXTv2.go

--- TXTv2.go
+++ TXTv2.go
@@ -60,3 +60,22 @@ func copy1(wg *sync.WaitGroup) {
 		}
 	}
 }
+
+func copy2(wg *sync.WaitGroup) {
+	defer wg.Done()
+	for x := 750; x <= 1499; x++ {
+		sFile, err := os.Open("./TXT/1.txt")
+		if err != nil {
+			log.Fatal(err)
+		}
+		eFile, err := os.Create(fmt.Sprintf("./TXT/%v.txt", x+1))
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer eFile.Close()
+		_, err = io.Copy(eFile, sFile)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+}
